276c13b69c8821d33e02ae4c8ba31c18444feced
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-11-13 18:39:16 +0100

    commit btnback

--- app/src/main/java/com/example/seminariointerfaces/MainActivity11.kt
+++ app/src/main/java/com/example/seminariointerfaces/MainActivity11.kt
@@ -2,10 +2,25 @@ package com.example.seminariointerfaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seminariointerfaces.databinding.ActivityMain10Binding
+import com.example.seminariointerfaces.databinding.ActivityMain11Binding
 
 class MainActivity11 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain11Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main11)
+
+        binding = ActivityMain11Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configurarBotonBack()
+    }
+
+    private fun configurarBotonBack() {
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/seminariointerfaces/MainActivity12.kt
+++ app/src/main/java/com/example/seminariointerfaces/MainActivity12.kt
@@ -2,10 +2,25 @@ package com.example.seminariointerfaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seminariointerfaces.databinding.ActivityMain10Binding
+import com.example.seminariointerfaces.databinding.ActivityMain12Binding
 
 class MainActivity12 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain12Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main12)
+
+        binding = ActivityMain12Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configurarBotonBack()
+    }
+
+    private fun configurarBotonBack() {
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
     }
 }
