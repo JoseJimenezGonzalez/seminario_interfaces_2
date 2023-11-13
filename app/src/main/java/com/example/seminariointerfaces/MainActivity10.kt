dee3fa0b73db7e2e7d6c3bbab1a1b86a3f954e41
JoseJimenezGonzalez <josexisco1993@gmail.com>
2023-11-13 18:17:36 +0100

    commit ok

--- app/src/main/java/com/example/seminariointerfaces/MainActivity10.kt
+++ app/src/main/java/com/example/seminariointerfaces/MainActivity10.kt
@@ -1,11 +1,26 @@
 package com.example.seminariointerfaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seminariointerfaces.databinding.ActivityMain10Binding
+import com.example.seminariointerfaces.databinding.ActivityMain7Binding
 
 class MainActivity10 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain10Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main10)
+
+        binding = ActivityMain10Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configurarBotonBack()
     }
+    private fun configurarBotonBack() {
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
+    }
+
 }
